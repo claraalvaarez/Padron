9932158c1f0ff3821e0441577b31b91ab321ebb7
Clara Álvarez Rodríguez <claraaalvarez23@gmail.com>
2023-10-09 11:03:41 +0100

    Reorganizar librerias

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,5 +1,16 @@
 package aplicacion;
 import presentacion.InterfazUsuario;
+import java.util.Scanner;
+public class SimpleTesting{
+	public static void main(String[] args) {
+		System.out.println("Enter Input : ");
+		Scanner scanner = new Scanner (System.in);
+		int a = scanner.nextInt();
+		System.out.println(a);
+		scanner.close();
+	}
+	}
+
 public class Principal{
 	public static void main(String[] args){ 
 		InterfazUsuario.ejecutar(args);
